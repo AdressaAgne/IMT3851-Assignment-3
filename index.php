b49a0bc2d209e981802ffc3610e9e628f7ebc40c
Agne <agne240@me.com>
2016-04-25 21:57:54 +0200

    fixing grid bug

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body> 
 <div class="row">
     <div class="col-8 col--centered">
-        <div class="col-l-3 col-4 col-m-6 col-s-12">
+        <div class="col-l-3 col-4 col-m-6 col-s-6">
             <article class="paper-card">
                 <header class="paper-card__header">
                     <h2 class="paper-card__header__text">Article header</h2>
@@ -23,7 +23,7 @@
                 </footer>
             </article>
         </div>
-        <div class="col-l-3 col-4 col-m-6 col-s-12">
+        <div class="col-l-3 col-4 col-m-6 col-s-6">
             <article class="paper-card">
                 <header class="paper-card__header">
                     <h2 class="paper-card__header__text">Article header</h2>
@@ -36,7 +36,7 @@
                 </footer>
             </article>
         </div>
-        <div class="col-l-3 col-4 col-m-6 col-s-12">
+        <div class="col-l-3 col-4 col-m-6">
             <article class="paper-card">
                 <header class="paper-card__header">
                     <h2 class="paper-card__header__text">Article header</h2>
@@ -49,7 +49,20 @@
                 </footer>
             </article>
         </div>
-        <div class="col-l-3 col-4 col-m-6 col-s-12">
+        <div class="col-l-3 col-4 col-m-6">
+            <article class="paper-card">
+                <header class="paper-card__header">
+                    <h2 class="paper-card__header__text">Article header</h2>
+                </header>
+                <main class="paper-card_content">
+                    <p>Article Main</p>
+                </main>
+                <footer class="paper-card_footer">
+                    <a href="#">Article Footer</a>
+                </footer>
+            </article>
+        </div>
+        <div class="col-l-3 col-4 col-m-6">
             <article class="paper-card">
                 <header class="paper-card__header">
                     <h2 class="paper-card__header__text">Article header</h2>
