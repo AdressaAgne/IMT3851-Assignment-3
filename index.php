779b02d9b63a91aa37a268e233cae94d306c6ac7
Agne <agne240@me.com>
2016-04-26 02:43:52 +0200

    Added paper-card

--- index.php
+++ index.php
@@ -9,68 +9,74 @@
 </head>
 <body> 
 <div class="row">
-    <div class="col-8 col--centered">
+    <div class="col-8 col-m-10 col--center">
         <div class="col-l-3 col-4 col-m-6 col-s-6">
             <article class="paper-card">
-                <header class="paper-card__header">
-                    <h2 class="paper-card__header__text">Article header</h2>
+                <header class="paper-card__header" style="background-image:url(img/placeholder/1.jpg);">
+                    <h2 class="paper-card__header--text">Article header</h2>
                 </header>
-                <main class="paper-card_content">
-                    <p>Article Main</p>
+                <main class="paper-card__content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas dolorum sunt beatae corrupti, nam unde voluptatibus non doloribus, illo laudantium!</p>
                 </main>
-                <footer class="paper-card_footer">
+                <footer class="paper-card__footer">
                     <a href="#">Article Footer</a>
                 </footer>
             </article>
         </div>
         <div class="col-l-3 col-4 col-m-6 col-s-6">
             <article class="paper-card">
-                <header class="paper-card__header">
-                    <h2 class="paper-card__header__text">Article header</h2>
+                <header class="paper-card__header" style="background-image:url(img/placeholder/2.jpg);">
+                    <h2 class="paper-card__header--text">Article header</h2>
                 </header>
-                <main class="paper-card_content">
-                    <p>Article Main</p>
+                <main class="paper-card__content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque numquam eligendi nisi veniam sequi ab cum suscipit iusto, deserunt earum.</p>
                 </main>
-                <footer class="paper-card_footer">
+                <footer class="paper-card__footer">
                     <a href="#">Article Footer</a>
                 </footer>
             </article>
         </div>
         <div class="col-l-3 col-4 col-m-6">
             <article class="paper-card">
-                <header class="paper-card__header">
-                    <h2 class="paper-card__header__text">Article header</h2>
+                <header class="paper-card__header" style="background-image:url(img/placeholder/3.jpg);">
+                    <h2 class="paper-card__header--text">Article header</h2>
                 </header>
-                <main class="paper-card_content">
-                    <p>Article Main</p>
+                <main class="paper-card__content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem numquam sed possimus illo doloribus quos perferendis repudiandae deleniti fuga, reprehenderit.</p>
                 </main>
-                <footer class="paper-card_footer">
+                <footer class="paper-card__footer">
                     <a href="#">Article Footer</a>
                 </footer>
             </article>
         </div>
         <div class="col-l-3 col-4 col-m-6">
             <article class="paper-card">
-                <header class="paper-card__header">
-                    <h2 class="paper-card__header__text">Article header</h2>
-                </header>
-                <main class="paper-card_content">
-                    <p>Article Main</p>
+                <header class="paper-card__header" style="background-image:url(img/placeholder/4.jpg);"></header>
+                <main class="paper-card__content">
+                    <h2 class="paper-card__content--text">Article header <small>subtext</small></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque animi eos sint ipsum laborum, ab autem nostrum explicabo omnis, quos.</p>
                 </main>
-                <footer class="paper-card_footer">
+                <footer class="paper-card__footer">
                     <a href="#">Article Footer</a>
                 </footer>
             </article>
         </div>
         <div class="col-l-3 col-4 col-m-6">
             <article class="paper-card">
-                <header class="paper-card__header">
-                    <h2 class="paper-card__header__text">Article header</h2>
+                <header class="paper-card__header" style="background-image:url(img/placeholder/4.jpg);">
+                    <h2 class="paper-card__header--text">Article header</h2>
                 </header>
-                <main class="paper-card_content">
-                    <p>Article Main</p>
+                <footer class="paper-card__footer">
+                    <a href="#">Article Footer</a>
+                </footer>
+            </article>
+        </div>
+        <div class="col-l-3 col-4 col-m-6">
+            <article class="paper-card paper-card--no-image">
+                <main class="paper-card__content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque animi eos sint ipsum laborum, ab autem nostrum explicabo omnis, quos.</p>
                 </main>
-                <footer class="paper-card_footer">
+                <footer class="paper-card__footer">
                     <a href="#">Article Footer</a>
                 </footer>
             </article>
